6dc5e550e2f69f396b3654d2bebae5d31924c3c7
shogo yasumura <shogo.yasumura@lassic.co.jp>
2022-11-18 20:20:05 +0900

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,70 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+
+type IVideoRef = React.RefObject<HTMLVideoElement>[]
+type IVideosRef = React.MutableRefObject<IVideoRef>
+
+const local = ['/Camera.mp4', '/Screen.mp4']
 
 function App() {
+  const [videos, setVideos] = React.useState<string[]>([])
+  React.useEffect(() => setVideos(local), [])
+
+  const refs = React.useRef<IVideoRef>([])
+  videos.forEach((_, i) => {
+    refs.current[i] = React.createRef()
+  })
+  const element = videos.map((src, i) => (
+    <video
+      key={src}
+      src={src}
+      width='400'
+      ref={refs?.current?.[i]}
+      controls
+      onLoadedData={(e) => {
+        if (e.target instanceof HTMLVideoElement) e.target.volume = 0
+      }}
+    />
+  ))
+
+  const fn = React.useCallback(
+    (callback: (el: HTMLVideoElement) => void, refs: IVideosRef) => {
+      return () => {
+        for (const el of Array.from(refs?.current)) {
+          if (el.current) {
+            callback(el.current)
+          }
+        }
+      }
+    },
+    [],
+  )
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div>
+      {element}
+      <div>
+        <button
+          onClick={fn((el) => {
+            el.currentTime = 0
+            el.play()
+          }, refs)}
         >
-          Learn React
-        </a>
-      </header>
+          Play
+        </button>
+        <button onClick={fn((el) => el.pause(), refs)}>Pause</button>
+        <input
+          type='range'
+          min='0'
+          max='1'
+          step='0.1'
+          defaultValue='0'
+          onChange={(e) =>
+            fn((el) => (el.volume = Number(e.target.value)), refs)()
+          }
+        />
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
